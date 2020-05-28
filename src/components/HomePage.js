a810c8757e523d681fb5387e27c1c675aec52054
danielbrog <brog.daniel@gmail.com>
2020-05-28 10:26:29 -0400

    base code cleaned up for test

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,24 +1,11 @@
-import React from 'react'
-import SubHeader from '../components/SubHeader'
-import Section from '../components/Section'
-import Header from '../components/Header'
-
-import skillsInfo from '../assets/sectionInfo/skillsInfo'
-import projectInfo from '../assets/sectionInfo/projectInfo'
-import coursesInfo from '../assets/sectionInfo/coursesInfo'
-
-
+import React from "react";
+import SearchForm from "./SearchForm";
 
 const HomePage = () => (
-    <div>
-    <Header />
-    <SubHeader />
-        <div className="content">
-            <Section title="Projects" items={projectInfo}/>
-            <Section title="Skills" items={skillsInfo}/>
-            <Section title="Courses" items={coursesInfo}/>
-        </div>
-    </div>
-        )
+  <div>
+    <div className="content">HomePage</div>
+    <SearchForm />
+  </div>
+);
 
-export default HomePage
+export default HomePage;
---
+++ src/components/SearchForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SearchForm = () => (
+  <div>
+    <div className="content">Search Restaurants</div>
+    <input type="search" name="Search Cities" placeholder="City"></input>
+  </div>
+);
+
+export default SearchForm;
