5f485f4ee60285f0d89766249766bc49a13ccc6e
danielbrog <brog.daniel@gmail.com>
2020-05-29 02:25:07 -0400

    appRouting update

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch} from 'react-router-dom'
-import HomePage from '../components/HomePage'
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomePage from "../components/HomePage";
 
 const AppRouter = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={HomePage} />
+    </Switch>
+  </BrowserRouter>
+);
 
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={HomePage}/>
-            </Switch>
-        </BrowserRouter>
-
-)
-
-export default AppRouter
+export default AppRouter;
